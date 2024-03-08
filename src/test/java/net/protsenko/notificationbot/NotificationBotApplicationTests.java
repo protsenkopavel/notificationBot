344ca98943088909a3f68cae5f590953f5404bed
Pavel Protsenko <pavelprotsenko@outlook.com>
2024-03-08 12:39:12 +0300

    Project init

---
+++ src/test/java/net/protsenko/notificationbot/NotificationBotApplicationTests.java
@@ -0,0 +1,13 @@
+package net.protsenko.notificationbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
